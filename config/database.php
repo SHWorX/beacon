813d76325b0dc62d9640227a5d0c16d649ec47c6
Steffen Haase <shworx.development@gmail.com>
2026-08-12 15:47:32 +0800

    - Added support for PostgreSQL - Added missing foreign key statement in 'remember_tokens' migration. - Removed unnecessary 'unique()' call in the 'rate_limit' migration, since it's already covered/implied by the 'primary()' call.

--- config/database.php
+++ config/database.php
@@ -9,14 +9,21 @@
 
 use App\Support\Env;
 
-return [
+$driver = Env::get('DB_DRIVER', 'mysql');
+
+$config = [
     'driver'    => Env::get('DB_DRIVER', 'mysql'),
     'host'      => Env::get('DB_HOST', 'localhost'),
-    'port'      => Env::get('DB_PORT', '3306'),
+    'port'      => Env::get('DB_PORT', $driver === 'pgsql' ? '5432' : '3306'),
     'database'  => Env::get('DB_DATABASE'),
     'username'  => Env::get('DB_USERNAME'),
     'password'  => Env::get('DB_PASSWORD'),
-    'charset'   => Env::get('DB_CHARSET', 'utf8mb4'),
-    'collation' => Env::get('DB_COLLATION', 'utf8mb4_unicode_520_ci'),
+    'charset'   => Env::get('DB_CHARSET', $driver === 'pgsql' ? 'utf8' : 'utf8mb4'),
     'prefix'    => Env::get('DB_PREFIX', ''),
 ];
+
+if ($driver === 'mysql') {
+    $config['collation'] = Env::get('DB_COLLATION', 'utf8mb4_unicode_ci');
+}
+
+return $config;
